325d6e3a91b487e9092e27743680f99f6bab5d2b
GP Orcullo <kinsamanka@gmail.com>
2015-11-22 20:47:08 +0700

    initial commit

---
+++ build_mk.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd /usr/src/machinekit/src
+./autogen.sh
+./configure \
+	CC=arm-linux-gnueabihf-gcc \
+	CXX=arm-linux-gnueabihf-g++ \
+	--with-posix \
+	--without-rt-preempt \
+	--without-xenomai \
+	--without-xenomai-kernel \
+	--without-rtai-kernel
+make
