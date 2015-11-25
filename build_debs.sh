96ed553bc6e138e09ea7cf274ed9b1a97aecac7f
GP Orcullo <kinsamanka@gmail.com>
2015-11-25 21:07:05 +0700

    split up flavors

--- build_debs.sh
+++ build_debs.sh
@@ -5,12 +5,23 @@ if echo ${TAG} | grep -iq arm; then
 	PROOT_OPTS="${PROOT_OPTS} -q qemu-arm-static"
 fi
 
+case "${FLAVOR}" in
+   "posix") option="-p"
+   ;;
+   "rt_preempt") option="-r" 
+   ;;
+   "xenomai") option="-x" 
+   ;;
+   *) option="-prx" 
+   ;;
+esac
+
 proot ${PROOT_OPTS} -r ${ROOT} \
 	sh -exc 'cd /usr/src/; \
 		 mkdir -p build/${MK_DIR}; \
 		 cp -a machinekit build/${MK_DIR}; \
 		 cd build/${MK_DIR}/machinekit; \
-		 ./debian/configure -prx; \
+		 ./debian/configure ${option}; \
 		 debuild -eDEB_BUILD_OPTIONS="parallel=${JOBS}" \
 		 	-us -uc -b -j${JOBS}'
 
