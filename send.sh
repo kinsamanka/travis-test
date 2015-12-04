2b662d4a857518f8eebd307159ecc3375a0d8853
GP Orcullo <kinsamanka@gmail.com>
2015-12-05 07:53:40 +0800

    4

--- send.sh
+++ send.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sftp -i id_rsa -P 9022 -oBatchMode=no -b - mktravis@deb.mah.priv.at << !
+yes | sftp -i id_rsa -P 9022 -oBatchMode=no -b - mktravis@deb.mah.priv.at << !
 cd shared
 put ab
 bye
