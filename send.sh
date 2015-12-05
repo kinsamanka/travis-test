e6619a66b78d80d22c8ba7f3974c6e751316b93d
GP Orcullo <kinsamanka@gmail.com>
2015-12-05 09:32:01 +0800

    19

--- send.sh
+++ send.sh
@@ -1,13 +1,8 @@
 #!/bin/sh -ex
-openssl aes-256-cbc -K $encrypted_f45b3652c5fe_key -iv $encrypted_f45b3652c5fe_iv -in dat.tgz.enc -out dat.tgz -d
-tar xvf dat.tgz -C ~/
-ls -l > ab
+find . > ab
 chmod a+rw ab
-echo $(pwd)
-find .
-sftp -P 9022 -oBatchMode=no -b - mktravis@deb.mah.priv.at << !
+sftp -i id_rsa -P 9022 -o StrictHostKeyChecking=no -oBatchMode=no -b - mktravis@deb.mah.priv.at << !
 cd shared
-put /home/travis/.ssh/known_hosts
 put ab
 bye
 !
