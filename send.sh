57b983f8cc052341ff630ad99aa6765d76a479af
GP Orcullo <kinsamanka@gmail.com>
2015-12-05 22:09:26 +0800

    22

--- send.sh
+++ send.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 find . > ab
 chmod a+rw ab
-sftp -i ~/access_key -P 9022 -o StrictHostKeyChecking=no -oBatchMode=no -b - mktravis@deb.mah.priv.at << !
+sftp -i ~/access_key -P 9022 -o StrictHostKeyChecking=no -oBatchMode=no -b - travis@deb.mah.priv.at << !
 cd shared
 put ab
 bye
