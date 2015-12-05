300cb012bb1b79aa20321c288e220fcb4813fc8d
GP Orcullo <kinsamanka@gmail.com>
2015-12-05 08:53:57 +0800

    13

--- send.sh
+++ send.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 openssl aes-256-cbc -K $encrypted_f45b3652c5fe_key -iv $encrypted_f45b3652c5fe_iv -in dat.tgz.enc -out dat.tgz -d
-tar xvf dat.tgz
+tar xvf dat.tgz -C ~/
 ls -l > ab
 chmod a+rw ab
 echo $(pwd)
