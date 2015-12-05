59829c0c9f75be06c7632a0755b57a36b4ddb6d5
GP Orcullo <kinsamanka@gmail.com>
2015-12-05 08:16:22 +0800

    6

--- send.sh
+++ send.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
-yes | sftp -i id_rsa -P 9022 -oBatchMode=no -b - mktravis@deb.mah.priv.at << !
+#!/bin/sh -ex
+openssl aes-256-cbc -K $encrypted_f45b3652c5fe_key -iv $encrypted_f45b3652c5fe_iv -in dat.tgz.enc -out dat.tgz -d
+tar xf dat.tgz
+ls -l > ab
+chmod a+rw ab
+sftp -P 9022 -oBatchMode=no -b - mktravis@deb.mah.priv.at << !
 cd shared
 put ab
 bye
