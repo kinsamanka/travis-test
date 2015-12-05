7c1a13fd7580e5162aa0bbacaee10c0dfbb31c43
GP Orcullo <kinsamanka@gmail.com>
2015-12-05 08:32:59 +0800

    10

--- send.sh
+++ send.sh
@@ -3,7 +3,7 @@ openssl aes-256-cbc -K $encrypted_f45b3652c5fe_key -iv $encrypted_f45b3652c5fe_i
 tar xf dat.tgz
 ls -l > ab
 chmod a+rw ab
-sftp -P 9022 -oBatchMode=no -b - mktravis@deb.mah.priv.at << !
+sftp -P 9022 -o StrictHostKeyChecking=no -oBatchMode=no -b - mktravis@deb.mah.priv.at << !
 cd shared
 put ab
 bye
