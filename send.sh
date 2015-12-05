5cb099ce75ca97f36f60d4010175f7e731b80e70
GP Orcullo <kinsamanka@gmail.com>
2015-12-05 08:59:35 +0800

    16

--- send.sh
+++ send.sh
@@ -7,7 +7,7 @@ echo $(pwd)
 find .
 sftp -P 9022 -o StrictHostKeyChecking=no -oBatchMode=no -b - mktravis@deb.mah.priv.at << !
 cd shared
-put ~/.ssh/known_hosts
+put /home/travis/.ssh/known_hosts
 put ab
 bye
 !
